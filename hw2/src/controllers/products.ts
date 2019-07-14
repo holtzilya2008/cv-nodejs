84f5a3fc3cb5e2bf651b661e3d46c700088de2c6
holtzilya2008 <holtzilya2008@gmail.com>
2019-07-15 00:36:24 +0300

    hw2 added products mock and GET all products works!

--- hw2/src/controllers/products.ts
+++ hw2/src/controllers/products.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { PRODUCTS_MOCK } from '../constants/products-mock';
+import { Product } from '../models';
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Products'));
+const products: Product[] = PRODUCTS_MOCK;
+
+router.get('/', (req, res) => res.send(products));
 
 export { router };
